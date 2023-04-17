dbb005f671c02a95137e662009e6b37546d43c66
root <lavendah17@gmail.com>
2023-04-17 14:11:22 +0000

    task 4

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,11 +1,10 @@
 #!/usr/bin/node
 // get an array of all command line arguments passed to the script, excluding the first two arguments
-const args = process.argv.slice(2);
-const x = args.length;
-if (x === 2) {
-  console.log('ArgumentS found');
-} else if (x === 1) {
+const args = process.argv.slice(2)
+if (args.length === 0) {
+  console.log('No argument');
+} else if (args.length === 1) {
   console.log('Argument found');
 } else {
-  console.log('No argument');
+  console.log('Arguments found');
 }
