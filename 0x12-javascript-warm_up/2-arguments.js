5e9d7cc06b0556865c5b0cadfa405c790f3e6c3a
root <lavendah17@gmail.com>
2023-04-17 11:21:23 +0000

    task 2

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const x = 'number';
+if (x === 0) {
+  console.log('No argument');
+} else if (x === 1) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}
