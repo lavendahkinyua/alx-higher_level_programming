4f1db9691cefbf1cbeda789ecfb59ffded586503
lavendahkinyua <lavendah17@gmail.com>
2023-05-03 14:07:40 -0700

    solutions to tasks

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-# a Python script that fetches https://alx-intranet.hbtn.io/status
+"""Fetches https://alx-intranet.hbtn.io/status."""
 import urllib.request
 if __name__ == "__main__":
     request = urllib.request.Request("https://alx-intranet.hbtn.io/status")
