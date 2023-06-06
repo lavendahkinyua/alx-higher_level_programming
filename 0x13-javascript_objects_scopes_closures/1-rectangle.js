689bd7472215dfe8342f67d82b40e8a459eee0ba
lavendahkinyua <lavendah17@gmail.com>
2023-06-06 09:22:03 -0700

    task 1

---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+/**
+ * class rectangle with constructor height and width
+ */
+class Rectangle {
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
+  }
+}
+module.exports = Rectangle;
