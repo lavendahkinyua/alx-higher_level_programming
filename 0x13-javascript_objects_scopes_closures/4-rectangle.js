bb937797951d3a8fe2be9567e8edfd73c99b8792
lavendahkinyua <lavendah17@gmail.com>
2023-06-06 10:32:46 -0700

    task 4

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,40 @@
+#!/usr/bin/node
+/**
+ *instance that exhanges width and height
+ */
+class Rectangle {
+  constructor(w, h) {
+    if (typeof w === 'number' && w > 0 && typeof h === 'number' && h > 0) {
+      this.width = w;
+      this.height = h;
+    } else {
+      // Create an empty object if width or height is invalid
+      this.width = 0;
+      this.height = 0;
+    }
+  }
+
+  print() {
+    for (let i = 0; i < this.height; i++) {
+      let row = '';
+      for (let j = 0; j < this.width; j++) {
+        row += 'X';
+      }
+      console.log(row);
+    }
+  }
+
+  rotate() {
+    const temp = this.width;
+    this.width = this.height;
+    this.height = temp;
+  }
+
+  double() {
+    this.width *= 2;
+    this.height *= 2;
+  }
+}
+
+module.exports = Rectangle;
+
