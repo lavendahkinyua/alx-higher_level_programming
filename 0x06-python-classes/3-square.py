a4ac445969200f77a86327c5ba57ad7243f46805
lavendahKinyua <lavendah17@gmail.com>
2023-07-04 22:43:03 -0700

    task 3

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+class square:
+    def __innit__(self, _size=0)
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self._size = size
+    def __area__(self):
+        return self._size ** 2
