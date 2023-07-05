78df97c451499493e55697d2caa0dc60c17bf39a
lavendahKinyua <lavendah17@gmail.com>
2023-07-05 02:10:07 -0700

    task 0

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -2,15 +2,7 @@
 """defines a class square"""
 class Square:
     """
-    Class representing a square
+    empty class
     """
-    def __init__ (self, length):
-        """
-        Initializes a square with the given length
-        """
-        self.length = length
-    def get_area (self):
-        return self.length ** 2
-    def get_perimeter(self):
-        return 4 * self.length
+    pass
         
