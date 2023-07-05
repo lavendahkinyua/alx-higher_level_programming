27cacd9250b388d5fe9d3a4bf224f084f3fd8dfc
lavendahKinyua <lavendah17@gmail.com>
2023-07-05 03:03:08 -0700

    task3

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python3
-class square:
-    def __innit__(self, _size=0)
+""" A class Square"""
+
+
+class Square:
+""" Initializing the class"""
+    def __init__(self, size=0)
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
         else:
-            self._size = size
+            self.__size = size
     def __area__(self):
-        return self._size ** 2
+        return self.__size ** 2
