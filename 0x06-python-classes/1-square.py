4e56987f881ddc3786ecf7e67bb1be3775091f1d
lavendahKinyua <lavendah17@gmail.com>
2023-07-05 02:43:35 -0700

    task 1

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 """defines a class square"""
-class square:
+
+
+class Square:
 """ defines a square"""
     def __innit__(self, _size):
+        """ INITIALIZING A SQUARE CLASS"""
         self._size = size
