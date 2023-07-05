4bba5b649d9daaa519e7f6178ae1e497127b8e52
lavendahKinyua <lavendah17@gmail.com>
2023-07-05 02:14:25 -0700

    task0

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 """defines a class square"""
-class Square:
-    """
-    empty class
-    """
-    pass
-        
+  class Square:
+      """
+      empty class
+      """
+      pass
