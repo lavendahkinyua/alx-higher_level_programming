6d146fdf15b84e08c84201a64e7ae825f655e01e
lavendahKinyua <lavendah17@gmail.com>
2023-07-04 21:55:56 -0700

    task 0

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+class square :
+    def __innit__ (self, length):
+        self.length = lenghth
+    def get_area (self):
+        return self.length ** 2
+    def get_perimeter(self):
+        return 4 * self.length
+        
---
+++ 0x06-python-classes/README.md
@@ -0,0 +1,1 @@
+pythonc classes and objects
